586381d4fc4d3e22967b2fe41572fe70a2405827
littlesluggerboi <tayaotaoaeron@gmail.com>
2025-04-21 22:59:30 +0800

    Added routes for comments.

---
+++ routes/commentRoutes.js
@@ -0,0 +1,42 @@
+import { Router } from "express";
+import idValidationMiddleWare from "../middlewares/idValidation.js";
+import passport from "passport";
+import {
+  commentsPostValidator,
+  commentsUpdateValidator,
+} from "../validator/commentsValidator.js";
+import validator from "../validator/validator.js";
+import commentController from "../controller/commentController.js";
+
+const commentRoutes = Router();
+
+commentRoutes.get("/", commentController.getComments);
+
+commentRoutes.get("/:id", idValidationMiddleWare, commentController.getComment);
+
+commentRoutes.post(
+  "/",
+  passport.authenticate("jwt"),
+  commentsPostValidator,
+  validator,
+  commentController.postComment
+);
+
+commentRoutes.put(
+  "/:id",
+  passport.authenticate("jwt"),
+  idValidationMiddleWare,
+  commentController.isAuthorized,
+  commentsUpdateValidator,
+  commentController.updateComment
+);
+
+commentRoutes.delete(
+  "/:id",
+  passport.authenticate("jwt"),
+  idValidationMiddleWare,
+  commentController.isAuthorized,
+  commentController.deleteComment
+);
+
+export default commentRoutes;
