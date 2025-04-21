44533f85984699255b2f8264f9064adf8e6a7812
littlesluggerboi <tayaotaoaeron@gmail.com>
2025-04-21 20:29:34 +0800

    Added middleware for int-id params validation.

---
+++ middlewares/idValidation.js
@@ -0,0 +1,13 @@
+import { param, validationResult } from "express-validator";
+
+const idValidationMiddleWare = [
+  param("id").isInt(),
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res.status(404).json({ message: "Resource not found." });
+    next();
+  },
+];
+
+export default idValidationMiddleWare;
